db3ddb624b083fdfc9006048982431c594f93176
aymane boujeddi <aymane655@gmail.com>
2025-01-15 16:34:09 +0100

    add(auth) : added the checking for each user and the redirect for them to a specified page

--- Actions/signIn.php
+++ Actions/signIn.php
@@ -6,9 +6,31 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 $email = $_POST['email'];
 $password = $_POST["password"];
 
-$loginUser = new User("","","","");
+$loginUser = new User("","","","","");
 $userInfo = $loginUser->login($email,$password);
-var_dump($userInfo);
 
+session_start();
+if($userInfo['role'] == "teacher"){
+    $_SESSION['id'] = $userInfo['userID'];
+    $_SESSION['role'] = $userInfo['role'];
+    $_SESSION['username'] = $userInfo['username'];
+    header("location: ../Views/teacher.php");
+    exit();
+}elseif($userInfo['role'] == "admin"){
+    $_SESSION['id'] = $userInfo['userID'];
+    $_SESSION['role'] = $userInfo['role'];
+    $_SESSION['username'] = $userInfo['username'];
+    header("location: ../Views/admin.php");
+    exit();
+}elseif($userInfo['role'] == "student"){
+    $_SESSION['id'] = $userInfo['userID'];
+    $_SESSION['role'] = $userInfo['role'];
+    $_SESSION['username'] = $userInfo['username'];
+    header("location: ../index.php");
+    exit();
+}elseif(empty($userInfo)){
+    $_SESSION['errors'] = $loginUser->getErrors();
+    header("location: ../Views/login.php");
+}
 
 }
