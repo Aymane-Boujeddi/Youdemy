2e9b831c5c70144c655fd1fee4999b147378e417
aymane boujeddi <aymane655@gmail.com>
2025-01-17 11:54:19 +0100

    YOUD-11 add(course) : Added the Class CourseVideo for the management of the video content

--- Actions/signIn.php
+++ Actions/signIn.php
@@ -8,24 +8,21 @@ $password = $_POST["password"];
 
 $loginUser = new User("","","","","");
 $userInfo = $loginUser->login($email,$password);
-
 session_start();
+$_SESSION['id'] = $userInfo['userID'];
+$_SESSION['role'] = $userInfo['role'];
+$_SESSION['username'] = $userInfo['username'];
+
 if($userInfo['role'] == "teacher"){
-    $_SESSION['id'] = $userInfo['userID'];
-    $_SESSION['role'] = $userInfo['role'];
-    $_SESSION['username'] = $userInfo['username'];
+   
     header("location: ../Views/teacher.php");
     exit();
 }elseif($userInfo['role'] == "admin"){
-    $_SESSION['id'] = $userInfo['userID'];
-    $_SESSION['role'] = $userInfo['role'];
-    $_SESSION['username'] = $userInfo['username'];
+   
     header("location: ../Views/admin.php");
     exit();
 }elseif($userInfo['role'] == "student"){
-    $_SESSION['id'] = $userInfo['userID'];
-    $_SESSION['role'] = $userInfo['role'];
-    $_SESSION['username'] = $userInfo['username'];
+
     header("location: ../index.php");
     exit();
 }elseif(empty($userInfo)){
